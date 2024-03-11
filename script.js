768980bb01ba15ca5afd016a970afb18f55af20b
swayam suryavanshi <codingswayam24@gmail.com>
2024-03-11 11:07:27 +0530

    Step 1 updated

--- script.js
+++ script.js
@@ -15290,17 +15290,14 @@ const targetWords = [
     "shave",
   ];
 
+// Constants for game setup
 
-const WORD_LENGTH = 5;
-const FLIP_ANIMATION_DURATION = 500;
-const DANCE_ANIMATION_DURATION = 500;
-const keyboard = document.querySelector("[data-keyboard]");
-const alertContainer = document.querySelector("[data-alert-container]");
-const guessGrid = document.querySelector("[data-guess-grid]");
-const offsetFromDate = new Date(2022, 0, 1);
-const msOffset = Date.now() - offsetFromDate;
-const dayOffset = msOffset / 1000 / 60 / 60 / 24;
-const targetWord = targetWords[Math.floor(dayOffset)];
+
+// DOM elements for interaction
+
+
+
+
 
 startInteraction();
 console.log("jhi");
